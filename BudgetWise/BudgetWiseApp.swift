cd443aa00e383f1c227fe6e8dd3a629cc1efb643
Tanjin Sezan <tanjinsizan51@gmail.com.com>
2023-08-07 14:37:35 +0600

    Initial Commit

---
+++ BudgetWise/BudgetWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetWiseApp.swift
+//  BudgetWise
+//
+//  Created by Tanjin Rashid Sezan on 8/7/23.
+//
+
+import SwiftUI
+
+@main
+struct BudgetWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BudgetWise/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BudgetWise
+//
+//  Created by Tanjin Rashid Sezan on 8/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
